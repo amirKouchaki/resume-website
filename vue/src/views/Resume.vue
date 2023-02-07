17f49907bdbf00f3a7f5018280a2e074582f1eaf
Amir Kouchaki <amirkouchaki1@gmail.com>
2023-02-08 02:16:59 +0330

    added hero section but the layout is still unresponsive

--- vue/src/views/Resume.vue
+++ vue/src/views/Resume.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <article class="container">
         <header class="resume-header">
             <div class="header-profile-info">
                 <img
@@ -21,12 +21,30 @@
                 <router-link :to="{ name: 'resume' }">Extra</router-link>
             </nav>
         </header>
-    </div>
+        <section class="hero-section">
+            <img src="../assets/images/profile.jpg" alt="" class="hero-img" />
+            <div class="hero-info">
+                <p class="short-intro">Web Designer</p>
+                <h2 class="hero-name">Amir Kouchaki</h2>
+                <p class="hero-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quam illum excepturi assumenda magnam, error, eligendi
+                    suscipit placeat vitae, tempora perferendis atque? Ipsa odit
+                    veritatis repellat eveniet fuga quae.
+                </p>
+                <div class="hero-btns">
+                    <button class="hero-cv-btn hero-btn">Download CV</button
+                    ><button class="hero-btn hero-contact-btn">Contact</button>
+                </div>
+            </div>
+        </section>
+    </article>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
+@use "../abstracts" as *;
 .resume-header {
     display: flex;
     align-items: center;
@@ -56,5 +74,66 @@
 .main-nav {
     display: flex;
     gap: 2.7em;
+}
+
+.hero-section {
+    display: flex;
+    gap: 3em;
+    justify-content: center;
+    align-items: center;
+    padding-block: 5em;
+
+    .hero-info {
+        max-width: 53ch;
+    }
+}
+
+.hero-img {
+    width: 200px;
+    border-radius: 999px;
+    object-position: left;
+    box-shadow: 0 0 23px 0 rgba(0, 0, 0, 0.8);
+    border: 10px solid $hero-border-color;
+}
+
+.short-intro {
+    color: $secondary-text-color;
+}
+
+.hero-name {
+    font-size: xx-large;
+}
+
+.hero-description {
+    color: $long-text-color;
+    padding-block: 1.7em;
+}
+
+.hero-btns {
+    display: flex;
+    gap: 0.7em;
+}
+
+.hero-btn {
+    background-color: $main-bg-color;
+    color: $main-text-color;
+    $border-color: $secondary-button-border-color;
+    border: 1.1px solid $border-color;
+    padding: 0.45em 1.4em;
+    border-radius: 1.2em;
+
+    &:hover {
+        background-color: $border-color;
+        color: $o-main-text-color;
+        transition: all 250ms ease-in-out;
+    }
+}
+
+.hero-cv-btn {
+    $border-color: $main-button-border-color;
+    border: 1.1px solid $border-color;
+    &:hover {
+        background-color: $border-color;
+    }
 }
 </style>
