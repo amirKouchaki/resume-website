11d952f43f4c0d946023d481d269d6a9057039d9
Amir Kouchaki <amirkouchaki1@gmail.com>
2023-09-23 13:11:46 +0330

    fixing push error on gh-pages branch in the vue/deploy.sh

--- vue/deploy.sh
+++ vue/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push git@github.com:amirKouchaki/resume-website.git main:gh-pages
+git push -f git@github.com:amirKouchaki/resume-website.git master:gh-pages
 cd -
